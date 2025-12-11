2af26f621d82e761cedffa22d253ebe1a4a9d113
khoerul-mutaqin <khoerul.mutaqin@falinwa.com>
2025-12-11 11:59:53 +0700

    [upd] feature auto validate next transfer dari int m2b

---
+++ cdp_action_auto_validate_next_transfer_dari_int_m2b/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "cdp_action_auto_validate_next_transfer_dari_int_m2b",
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'description': """
+Long description of module's purpose
+    """,
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+    # any module necessary for this one to work correctly
+    'depends': ['base','stock'],
+    # always loaded
+    'data': [
+        'data/views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+}
+
